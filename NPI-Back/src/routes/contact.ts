447254ec254f0ad91d34cb746d5ccbaa52555294
FayadIdrissB <fayad-idriss.bacari@outlook.com>
2025-06-15 22:57:39 +0200

    update

--- NPI-Back/src/routes/contact.ts
+++ NPI-Back/src/routes/contact.ts
@@ -1,12 +1,7 @@
 import { Router } from 'express';
 import { handleContact } from '../controller/contact';
 
-
 const router = Router();
-
-router.get('/', (req, res) => {
-  res.send('✅ Contact route OK');
-});
 
 router.post('/contact', handleContact);
 
