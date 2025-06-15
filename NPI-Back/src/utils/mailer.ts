8f7e018705c80c69e9c6955c9069daf94426b27a
FayadIdrissB <fayad-idriss.bacari@outlook.com>
2025-06-15 22:35:57 +0200

    uodate mailer

--- NPI-Back/src/utils/mailer.ts
+++ NPI-Back/src/utils/mailer.ts
@@ -3,4 +3,8 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
+if (!process.env.RESEND_API_KEY) {
+  throw new Error('Missing RESEND_API_KEY in environment variables');
+}
+
 export const resend = new Resend(process.env.RESEND_API_KEY);
