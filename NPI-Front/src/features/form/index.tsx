480d82084ae7648de090d632588ed21ce3cacbe1
FayadIdrissB <fayad-idriss.bacari@outlook.com>
2025-06-23 21:03:56 +0200

    update files

--- NPI-Front/src/features/form/index.tsx
+++ NPI-Front/src/features/form/index.tsx
@@ -23,7 +23,7 @@ export default function ContactForm({ onSubmitSuccess }: ContactFormProps) {
     setStatus('loading');
 
     try {
-      const res = await fetch('https://0a25-2001-861-8bb2-4bb0-7939-6872-73cb-b1ce.ngrok-free.app/api/contact', {
+      const res = await fetch('https://b0f9-2001-861-8bb2-4bb0-e1d7-6617-e67d-398d.ngrok-free.app/api/contact', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({
